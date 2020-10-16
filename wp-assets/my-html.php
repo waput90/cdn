78f1d0877dd7b9009af97fd5d0abdf31b010f542
James Tubiano <tubianojames@gmail.com>
2020-10-16 15:16:23 +0800

    Update my-html.php

--- wp-assets/my-html.php
+++ wp-assets/my-html.php
@@ -11,7 +11,7 @@
     <div ng-controller="test-controller">
     <input type="text" ng-model="inputData">
     <h4>{{inputData}}</h4>
-    TEST HERE
+    TEST HERE i am changed
         {{testdata}}
     </div>
 </body>
